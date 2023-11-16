00ec7f968e016896e045ac7d28262141b620d3cf
Ganesh <ganeshnimmala65@gmail.com>
2023-11-16 09:07:23 +0530

    Redux Store & API slice

---
+++ frontend/src/slices/apiSlice.js
@@ -0,0 +1,9 @@
+import { createAPI, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+export const apiSlice = createAPI({
+  baseQuery,
+  tagTypes: ["Product", "Order", "User"],
+  endpoints: (builder) => ({}),
+});
